90ba952fa387ab252f3c4d5270057e64a6fb9b94
Pranath Prasanth <pranathp05@gmail.com>
2026-04-02 21:56:59 +0530

    2nd commit

--- backend/services/ocr_service.py
+++ backend/services/ocr_service.py
@@ -4,4 +4,13 @@ import tempfile
 
 def extract_text(file):
     with tempfile.NamedTemporaryFile(delete=False) as temp:
-        
+        temp.write(file.file.read())
+        temp_path=temp.name
+
+    try:
+        image=Image.open(temp_path)
+        text=pytesseract.image_to_string(image)
+    finally:
+        import os
+        os.remove(temp_path)
+    return text
